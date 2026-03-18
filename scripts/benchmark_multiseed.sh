73bb83dbb32f8835d2094f8f5e0798cc1d157d1a
Hy Truong Son <sonpascal93@gmail.com>
2026-03-17 20:38:34 -0500

    Update scripts for BIOKDD 2026

--- scripts/benchmark_multiseed.sh
+++ scripts/benchmark_multiseed.sh
@@ -1,4 +1,4 @@
 python experiments/benchmark_multiseed.py \
-  --data artifacts/binary/gfp_10000_esm_binary_32.npz \
-  --seeds 0 1 2 3 4 5 6 7 8 9 \
-  --out artifacts/gfp_10000_32_multiseed.json
+  --data artifacts/binary_pca/gfp_10000_pca_binary_16.npz \
+  --seeds 0 1 2 3 4 \
+  --out artifacts/gfp_10000_pca_16_multiseed.json
--- scripts/figure1.sh
+++ scripts/figure1.sh
@@ -1,4 +1,4 @@
-python experiments/plot_figure1_errorbars.py \
-  --results-dir artifacts/multiseed \
-  --prefix gfp \
-  --output-name figure1_latent_dimension_errorbars.pdf
+python experiments/plot_figure2_latent_dimension.py \
+  --input artifacts/aggregated/full_summary.csv \
+  --outdir artifacts/figures
+
